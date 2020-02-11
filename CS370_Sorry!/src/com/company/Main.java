749e7886fd530967f7f3d5d480014aa467efde89
pnick <pnick0509@users.noreply.github.com>
2020-02-11 16:28:49 -0500

    Okay

--- CS370_Sorry!/src/com/company/Main.java
+++ CS370_Sorry!/src/com/company/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:CS370_Sorry!/src/com/company/Main.java
 package com.company;
 
 import javafx.application.Application;
@@ -103,3 +104,56 @@ public class Main extends Application {
         }
     }
 }
+=======
+package com.company;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.Random;
+
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) {
+        int rowNum = 16, colNum = 16, gridH = 16, gridW = 16;
+        GridPane squareGrid = new GridPane();
+        BorderPane gameBoard = new BorderPane();
+
+
+
+        Random rand = new Random();
+        Color[] colors = {Color.BLACK, Color.BLUE, Color.GREEN, Color.RED};
+
+        for(int row = 0; row<rowNum; row++){
+            for(int col = 0; col<colNum; col++){
+                int n = rand.nextInt(4);
+                Rectangle gameSquare = new Rectangle();
+                gameSquare.setHeight(60);
+                gameSquare.setWidth(60);
+                gameSquare.setStroke(Color.BLACK);
+                gameSquare.setStrokeWidth(3);
+                gameSquare.setFill(Color.WHITE);
+                squareGrid.setRowIndex(gameSquare, row);
+                squareGrid.setColumnIndex(gameSquare, col);
+                squareGrid.getChildren().addAll(gameSquare);
+            }
+        }
+
+        gameBoard.setCenter(squareGrid);
+
+        Scene scene = new Scene(gameBoard);
+
+        primaryStage.setTitle("Grid");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+}
+>>>>>>> parent of 325a1e1... Added cards/drawing, fixed minor bugs.:CS370_Sorry!/src/com/company/com/company/Main.java
