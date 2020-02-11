b0930fa40229636917e0b1e071f5ca9dcd0cbcc3
pnick <pnick0509@users.noreply.github.com>
2020-02-11 16:42:47 -0500

    Did I do it?

---
+++ src/com/company/Main.java
@@ -0,0 +1,105 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.Random;
+
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) {
+
+        //Grid and board parameters
+        int rowNum = 16, colNum = 16, gridH = 16, gridW = 16;
+        //Grid pane to hold the grid of squares, a borderbane to hold the grid or game board in
+        GridPane squareGrid = new GridPane();
+        BorderPane gameBoard = new BorderPane();
+
+        //For loop that constructs the gameboard by going through rows/columns
+        //As it goes through it checks for specific column and row values to put buttons in
+        for(int row = 0; row<rowNum; row++) {
+            for (int col = 0; col < colNum; col++) {
+                if ((row == 0) || (col == 0) || (row == 15) || (col == 15) || (col == 2 && row < 7) || (row == 13 && col < 7) || (col == 13 && row > 8) || (row == 2 && col > 8) ||
+                        (col == 4 && row == 1) || (row == 4 && col == 14) || (row == 14 && col == 11) || (col == 1 && row == 11)) {
+
+                    //Make a button, assign it the rectangle object/shape
+                    Button bt = new Button();
+                    Rectangle gameSquare = new Rectangle();
+                    bt.setShape(gameSquare);
+                    bt.setMaxSize(60, 60);
+                    bt.setMinHeight(50);
+                    bt.setMinWidth(50);
+
+                    //Set the button action, make sure the row/col are final before we go in- as temp variables
+                    int finalCol = col;
+                    int finalRow = row;
+                    bt.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle (ActionEvent event) {
+                            int click = getInput(finalRow,finalCol);
+                            if(click != -1) {
+                                System.out.println(click);
+                            }
+
+                        }
+                    });
+
+                    //Rectangle alterations
+                    gameSquare.setHeight(60);
+                    gameSquare.setWidth(60);
+                    gameSquare.setStroke(Color.BLACK);
+                    gameSquare.setStrokeWidth(3);
+                    gameSquare.setFill(Color.WHITE);
+                    squareGrid.setRowIndex(bt, row);
+                    squareGrid.setColumnIndex(bt, col);
+                    squareGrid.getChildren().addAll(bt);
+                }
+            }
+        }
+
+
+        //Put the gameboard into the borderpane
+        gameBoard.setCenter(squareGrid);
+
+        //Put the borderpane into the scene
+        Scene scene = new Scene(gameBoard);
+
+        //Construct the scene and launch
+        primaryStage.setTitle("Sorry! v .01");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+
+    public int getInput(int row, int col){
+        if (row == 0) { //Top row
+            return col;
+        } else if (row == 15) { //Bottom row
+            return 45-col;
+        } else if (col == 15) { //Right col
+            return row+15;
+        } else if (col == 0) { //Left col
+            return 60-row;
+        } else if (col == 2 && row <=5){ // Top Left Home
+            return 59+row;
+        } else if (row == 13 && col <= 5) { // Bottom left home
+            return 74+col;
+        } else if (col == 13 && row >= 10){ // Bottom Right Home
+            return 84-row;
+        } else if (col >= 10 && row == 2 ){
+            return 79-col;
+        } else {
+            return -1; //Error case
+        }
+    }
+}
